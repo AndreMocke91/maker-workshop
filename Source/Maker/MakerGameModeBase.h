50f17c0994c21ed107fa47c390847790445640af
Andre Mocke <andremocke91@gmail.com>
2020-05-09 11:09:44 +0200

    Initial commit

---
+++ Source/Maker/MakerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MakerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAKER_API AMakerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
